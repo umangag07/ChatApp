412a162fa6d2d90604c5f55f25be5b8872f81792
umang <25umangagarwal@gmail.com>
2021-02-15 19:50:57 +0530

    Added css file for responsive design with rem unit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
+const PORT = process.env.PORT | 5000;
+const app =express()
+const server = http.createServer(app);
+const router = require('./router');
+
+
+// using socket io instance instead of http request
+const io = socketio(server);
+
+// using middleware 
+app.use(router);
+
+// managing the socket that is connected
+io.on('connection',(socket)=>{
+    console.log("we have a new connection")
+
+    //when user left
+    socket.on('disconnet',()=>{
+        console.log("user has left")
+    });
+
+})
+
+server.listen(PORT,()=>{
+    console.log(`server has started on the port ${PORT}`)
+})
+
